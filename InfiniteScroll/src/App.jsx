464f33bdb0f60edaeb7de30a31c669c5b82d94fb
jacktham96 <62689233+jacktham96@users.noreply.github.com>
2022-07-14 01:12:50 +0800

    Finish Infinte Scroll Project
    Build Infinite Scroller with React and React Hook

--- InfiniteScroll/src/App.jsx
+++ InfiniteScroll/src/App.jsx
@@ -1,11 +1,49 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import './index.css'
 
 function App() {
 
+  let offset = 0
+
+  const [pokemon , setPokemon] = useState([])
+
+  const  handleScroll = (event) => {
+    if (
+      window.innerHeight + event.target.documentElement.scrollTop +1 >=
+      event.target.documentElement.scrollHeight) {
+      console.log('end of page');
+      fetchData();
+    }
+  }
+
+  const fetchData = () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${offset}`)
+      .then(res => res.json())
+      .then(data => {
+        const newPokemon = []
+        data.results.forEach(pokemon => {
+          newPokemon.push(pokemon)
+        })
+        setPokemon(oldPokemon => [...oldPokemon , ...newPokemon])
+      })
+      offset += 10
+  }
+
+
+  useEffect(() => {
+      fetchData();
+      window.addEventListener('scroll', handleScroll)
+    },[])
+
   return (
     <div className="App">
-      <h1 className='text-3xl font-bold underline '>TEDST</h1>
+      {
+        pokemon.map((pokemon,i) => (
+          <div key={i} className='flex flex-col items-center justify-center'>
+            <h1 className='text-3xl font-bold border w-60 h-60 flex items-center'>{i + 1}. {pokemon.name}</h1>
+          </div>
+        ))
+      }
     </div>
   )
 }
